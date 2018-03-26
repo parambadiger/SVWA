6c5e86c1faa91fd75c0b215a17f97ff166d449be
Paramesh Badiger <Parameshbadiger245@gmail.com>
2018-03-26 20:32:12 +0530

    added index page

---
+++ index.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8" />
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <title>home</title>
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="stylesheet" href="css/main.css">
+    <script src="js/main.js"></script>
+</head>
+<body>
+    <header>
+        <div id="logo">
+            <h3>WOMEN'S ASSOCIATION</h3>
+        </div>
+        <ul>
+            <li><a href="http://">Contact Us</a></li>
+            <li><a href="http://">Facebook</a></li>
+            <li><a href="http://">Twitter</a></li>
+        </ul>
+    </header>
+</body>
+</html>
