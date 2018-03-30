4e78fdf8e49169632f6b8779447be5c68405464a
Paramesh Badiger <Parameshbadiger245@gmail.com>
2018-03-30 20:05:21 +0530

    modified index and stylesheet

--- index.php
+++ index.php
@@ -29,26 +29,28 @@
                     <div class="modal-body">
                         <div id="contact">
                             <center><h3>PLACE</h3></center>
-                            <div>
-                                <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d174343.69721440427!2d74.43494156925432!3d15.873354184675115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbf669f5095362f%3A0x7e34b31edcdefb5f!2sBelgaum%2C+Karnataka!5e0!3m2!1sen!2sin!4v1522345689921" width="100%" height="250" frameborder="0" allowfullscreen></iframe>
-                            </div>
-                            <div>
-                                <h4>ADDRESS:</h4>
-                                <p>Belgaum, Karnataka, India</p>
-                            </div>
-                            <div>
-                                <h4>CONTACT NO.:</h4>
-                                <div id="number">
-                                    <a href="#"><i class="fa fa-phone"></i> 1234567890</a>
-                                    <a href="#"><i class="fa fa-phone"></i> 1234567890</a>
+                            <div id="info">
+                                <div>
+                                    <iframe src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d174343.69721440427!2d74.43494156925432!3d15.873354184675115!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x3bbf669f5095362f%3A0x7e34b31edcdefb5f!2sBelgaum%2C+Karnataka!5e0!3m2!1sen!2sin!4v1522345689921" width="100%" height="250" frameborder="0" allowfullscreen></iframe>
                                 </div>
-                            </div>
-                            <div>
-                                <h4>FOLLOW US ON:</h4>
-                                <div id="follow">
-                                    <a href="#"><i class="fa fa-facebook-square fa-2x"></i>Facebook</a>
-                                    <a href="#"><i class="fa fa-twitter-square fa-2x"></i>Twitter</a>
-                                    <a href="#"><i class="fa fa-instagram fa-2x"></i>Instagram</a>
+                                <div id="detail">
+                                    <div>
+                                        <b>ADDRESS:</b>
+                                        <p>Belgaum, Karnataka, India</p>
+                                    </div>
+                                    <div>
+                                        <b>CONTACT NO.:</b>
+                                        <p><i class="fa fa-phone"></i><a href="#">1234567890</a></p>
+                                        <p><i class="fa fa-phone"></i><a href="#">1234567890</a></p>
+                                    </div>
+                                    <div>
+                                        <b>FOLLOW US ON:</b>
+                                        <p>
+                                            <a href="#"><i class="fa fa-facebook-square fa-2x"></i></a>
+                                            <a href="#"><i class="fa fa-twitter-square fa-2x"></i></a>
+                                            <a href="#"><i class="fa fa-instagram fa-2x"></i></a>
+                                        </p>
+                                    </div>
                                 </div>
                             </div>
                         </div>
